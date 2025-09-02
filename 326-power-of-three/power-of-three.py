5fd5a389d89413e71d36a592fb0a0aed69cd446a
Sophea Kong <109600860+sophea-kong@users.noreply.github.com>
2025-09-02 20:38:51 +0700

    Time: 7 ms (83.78%) | Memory: 12.5 MB (52.87%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -4,8 +4,11 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        result = n
-        while(result>1 and result%3==0):
-            result = result/3
-        return (result == 1)
+        while(n>1 and n%3==0):
+            n = n/3
+        return (n == 1)
+        # result = n
+        # while(result>1 and result%3==0):
+        #     result = result/3
+        # return (result == 1)
         
