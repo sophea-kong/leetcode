7304b05cf8d6aff30686be012dc21a1fa0a66be3
Sophea Kong <109600860+sophea-kong@users.noreply.github.com>
2025-09-05 14:35:32 +0700

    Time: 19 ms (5.08%) | Memory: 12.4 MB (68.87%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxNumberOfBalloons(self, text):
+        """
+        :type text: str
+        :rtype: int
+        """
+        balloon = "balloon"
+        text_dict = defaultdict(int)
+        for i in text:
+            if i in set(balloon):
+                text_dict[i] += 1
+        text_dict['l'] = int(text_dict['l']/2)
+        text_dict['o'] = int(text_dict['o']/2)
+        
+        min = text_dict['a']
+        for i in text_dict.values():
+            if(i<min):
+                min = i
+        return min
+        
