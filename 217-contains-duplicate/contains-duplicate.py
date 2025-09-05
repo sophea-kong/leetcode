47517e429ceadd9d96ca5becb66876fe36f3e9a2
Sophea Kong <109600860+sophea-kong@users.noreply.github.com>
2025-09-05 13:43:14 +0700

    Time: 21 ms (39.86%) | Memory: 25.9 MB (47.27%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums_set = set()
+        for i in nums:
+            if i in nums_set:
+                return True
+            else :
+                nums_set.add(i)
+        return False
+        
