55144861a82bb2e7cdc723bfe8eb62cad5c033ae
Sophea Kong <109600860+sophea-kong@users.noreply.github.com>
2025-09-05 14:39:46 +0700

    Time: 23 ms (5.08%) | Memory: 12.5 MB (68.87%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -9,8 +9,8 @@ class Solution(object):
         for i in text:
             if i in set(balloon):
                 text_dict[i] += 1
-        text_dict['l'] = int(text_dict['l']/2)
-        text_dict['o'] = int(text_dict['o']/2)
+        text_dict['l'] = (text_dict['l']//2)
+        text_dict['o'] = (text_dict['o']//2)
         
         mins = min(text_dict['b'],text_dict['a'],text_dict['l'],text_dict['o'],text_dict['n'])
         return mins
