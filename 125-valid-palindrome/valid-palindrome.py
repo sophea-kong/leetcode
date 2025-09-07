37b5edab6c9a97c40f7331080824301c1477301f
Sophea Kong <109600860+sophea-kong@users.noreply.github.com>
2025-09-07 14:19:28 +0700

    Time: 12 ms (77.94%) | Memory: 12.9 MB (70.18%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -7,11 +7,12 @@ class Solution(object):
         left,right = 0,len(s)-1
         while(left<=right):
             if(not s[left].isalnum()):
-                print(s[left])
                 left+=1
-            elif (not s[right].isalnum()):
+                continue
+            if (not s[right].isalnum()):
                 right-=1
-            elif ((s[left].lower() == s[right].lower())):
+                continue
+            if ((s[left].lower() == s[right].lower())):
                 left+=1
                 right-=1
             else:
