3341deb0f02098ab1ebd1914fc8b5117e0f8d491
Sophea Kong <109600860+sophea-kong@users.noreply.github.com>
2025-09-09 11:10:11 +0700

    Time: 20 ms (5.34%) | Memory: 12.6 MB (23.38%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        open_stk = list()
+        hsh = {')':'(','}':'{',']':'['}
+        for i in s:
+            if i in hsh.values():
+                open_stk.append(i)
+            else:
+                if(open_stk and open_stk[-1]==hsh[i]):
+                    open_stk.pop()
+                else:
+                    return False
+        print(open_stk)
+        return not open_stk
