2ca8d773605868b33ff4d8fa6116853143a43542
Sophea Kong <109600860+sophea-kong@users.noreply.github.com>
2025-09-11 20:53:40 +0700

    Time: 15 ms (61.91%) | Memory: 12.3 MB (78.84%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -11,9 +11,8 @@ class Solution(object):
         """
         left,right = 0,n
         while(left<right):
-            mid = (left+right)//2
-            if(isBadVersion(mid)):
-                right = mid
+            if(isBadVersion((left+right)//2)):
+                right = (left+right)//2
             else:
-                left = mid+1
+                left = (left+right)//2 +1
         return left
