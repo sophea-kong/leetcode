9898c57eb19796a22880bdadfe6df2c61344b34a
Sophea Kong <109600860+sophea-kong@users.noreply.github.com>
2025-09-12 19:33:19 +0700

    Time: 0 ms (100.00%) | Memory: 13 MB (11.96%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSubsequence(self, s, t):
+        S = len(s)
+        T = len(t)
+        if s == '': return True
+        if S > T: return False
+ 
+        j = 0
+        for i in range(T):
+            if t[i] == s[j]:
+                if j == S-1:
+                    return True
+                j += 1
+        
+        return False
+        
