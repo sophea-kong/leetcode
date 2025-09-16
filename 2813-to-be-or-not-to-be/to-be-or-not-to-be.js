086cc627b794826d8cf89e38bfa177f00d9e717e
Sophea Kong <109600860+sophea-kong@users.noreply.github.com>
2025-09-16 14:00:49 +0700

    Time: 53 ms (7.04%) | Memory: 53.1 MB (77.93%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe : (num)=>{
+            if (num !== val) throw new Error("Not Equal");
+            return true
+        },
+        notToBe : (num)=>{
+            if(num === val){
+                throw new Error("Equal");
+            }
+            return true
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
