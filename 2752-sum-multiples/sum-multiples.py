70c28f03d704edf3dcc691120fd226e9e2517f58
Sophea Kong <109600860+sophea-kong@users.noreply.github.com>
2025-09-28 22:06:17 +0700

    Time: 35 ms (50.46%) | Memory: 12.5 MB (12.88%) - LeetSync

---
+++ 2752-sum-multiples/sum-multiples.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def sumOfMultiples(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        sums = 0
+        for i in range(0,n+1):
+            if (i % 3 == 0) or (i%5==0) or (i%7==0):
+                sums += i
+
+        return sums
